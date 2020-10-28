b18467ea26548e508ded8ef6847034840595b9df
Amy Keneson <amy.l.keneson@icloud.com>
2020-10-28 17:37:16 -0500

    server.js to match class modules

--- server.js
+++ server.js
@@ -2,25 +2,34 @@
 // nosql module 06
 const express = require("express");
 const mongojs = require("mongojs");
+const { Mongoose } = require("mongoose");
+const
 
 // SETS UP THE EXPRESS APP
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 // SETS UP THE EXPRESS APP TO HANDLE DATA PARSING
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
 // STATIC DIRECTORY TO BE SERVED
-app.use(express.static("app/public"));
+app.use(express.static("public"));
+
+// MONGOOSE [nosql module 26]
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutDB", {
+    useNewurlParser: true,
+    useFindAndModify: false
+});
 
 // ROUTES
-require('./routes/apiRoutes');
+app.use(require('./routes/apiRoutes.js'));
+app.use(require('./routes/htmlRoutes.js'));
 
 // STARTS THE SERVER TO BEGIN LISTENING
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
-    console.log("http://localhost:8080");
+    console.log("http://localhost:3000");
 })
 
 
